4c030015c98be0c7f3a7e80fb10296114f923241
Xusratbek <matsapayevxusrat@gmail.com>
2025-02-21 17:14:54 +0500

    new

--- src/Home/Models.jsx
+++ src/Home/Models.jsx
@@ -2,55 +2,55 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
 const Models = () => {
-    const [setCars,cars]=useState([])
-    useEffect(()=>{
-      getCars()
+  const [setCars, cars] = useState([])
+  useEffect(() => {
+    getCars()
 
-    },[])
+  }, [])
 
-    const getCars=()=>{
-      axios({
-        url:"https://carsales-iysh.onrender.com/get_cars?category=lada",
-        method:"get"
-      }).then((res)=>{
-        console.log(res.data);
-        
-      })
-    }
-    
-    
+  const getCars = () => {
+    axios({
+      url: "https://carsales-iysh.onrender.com/get_cars?category=lada",
+      method: "get"
+    }).then((res) => {
+      console.log(res.data);
+
+    })
+  }
+
+
   return (
     <div></div>
-  //   <div className="p-6">
-  //   <div className="text-gray-600 text-sm mb-4">
-  //     <span>Bosh sahifa &gt; Modellari &gt; Chevrolet turlari</span>
-  //   </div>
+    //   <div className="p-6">
+    //   <div className="text-gray-600 text-sm mb-4">
+    //     <span>Bosh sahifa &gt; Modellari &gt; Chevrolet turlari</span>
+    //   </div>
 
-  //   <div className="flex justify-between mt-8">
-  //   <h1 className="text-4xl font-bold mb-8">Modellar turlari</h1>
+    //   <div className="flex justify-between mt-8">
+    //   <h1 className="text-4xl font-bold mb-8">Modellar turlari</h1>
 
-  //     <button className="bg-blue-500 text-white h-[50px] px-6 rounded-lg hover:bg-blue-600">
-  //       Admin o'tish
-  //     </button>
-  //   </div>
-  //   <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-  //     {cars.map((car) => (
-  //       <div key={car.id} className="bg-white shadow-md rounded-lg overflow-hidden">
-  //         <img
-  //           src={car.image} // Rasmlarni "public/images" papkasiga joylashtirish kerak
-  //           alt={car.name}
-  //           className="w-full h-48 object-cover"
-  //         />
-  //         <div className="p-4 text-center">
-  //           <h2 className="text-lg font-bold">{car.name}</h2>
-  //           <p className="text-gray-700">Narxi: {car.price}</p>
-  //         </div>
-  //       </div>
-  //     ))}
-  //   </div>
-    
-  // </div>
-   )
+    //     <button className="bg-blue-500 text-white h-[50px] px-6 rounded-lg hover:bg-blue-600">
+    //       Admin o'tish
+    //     </button>
+    //   </div>
+    //   <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    //     {cars.map((car) => (
+    //       <div key={car.id} className="bg-white shadow-md rounded-lg overflow-hidden">
+    //         <img
+    //           src={car.image} // Rasmlarni "public/images" papkasiga joylashtirish kerak
+    //           alt={car.name}
+    //           className="w-full h-48 object-cover"
+    //         />
+    //         <div className="p-4 text-center">
+    //           <h2 className="text-lg font-bold">{car.name}</h2>
+    //           <p className="text-gray-700">Narxi: {car.price}</p>
+    //         </div>
+    //       </div>
+    //     ))}
+    //   </div>
+
+    // </div>
+  )
 }
 
 export default Models
--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -7,78 +7,78 @@ const Register = () => {
     username: "",
     email: "",
     password: "",
-};
-const handleSubmit = async (values) => {
-  console.log(values); 
-  try {
+  };
+  const handleSubmit = async (values) => {
+    console.log(values);
+    try {
       const response = await auth.sign_up(values);
       if (response.status === 201) {
-          notification.success({
-              message: 'Registration Successful',
-              description: 'You have successfully registered.',
-          });
-          navigate("/");
+        notification.success({
+          message: 'Registration Successful',
+          description: 'You have successfully registered.',
+        });
+        navigate("/");
       }
-  } catch (error) {
-      console.log(error.response); 
+    } catch (error) {
+      console.log(error.response);
       notification.error({
-          message: 'Registration Failed',
-          description: error.response?.data?.message || 'An unexpected error occurred.',
+        message: 'Registration Failed',
+        description: error.response?.data?.message || 'An unexpected error occurred.',
       });
-  }
-};
+    }
+  };
 
-const onFinish = (values) => {
-  console.log('Received values of form: ', values);
-  handleSubmit(values); 
-};
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+    handleSubmit(values);
+  };
 
 
 
   return (
     <div className="flex h-screen">
-    {/* Left Section */}
-    <div className="w-1/2 bg-gradient-to-b from-blue-500 to-blue-700 text-white flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-bold mb-4">GoFinance</h1>
-      <p className="text-lg mb-6">The most popular peer to peer lending at SEA</p>
-      <button className="px-6 py-2 bg-blue-600 hover:bg-blue-800 rounded-full transition-all">Read More</button>
+      {/* Left Section */}
+      <div className="w-1/2 bg-gradient-to-b from-blue-500 to-blue-700 text-white flex flex-col justify-center items-center">
+        <h1 className="text-4xl font-bold mb-4">GoFinance</h1>
+        <p className="text-lg mb-6">The most popular peer to peer lending at SEA</p>
+        <button className="px-6 py-2 bg-blue-600 hover:bg-blue-800 rounded-full transition-all">Read More</button>
+      </div>
+
+      {/* Right Section */}
+      <div className="w-1/2 bg-white flex flex-col justify-center items-center">
+        <h2 className="text-3xl font-bold mb-4">Hello!</h2>
+        <p className="mb-8">Sign Up to Get Started</p>
+        <form initialValues={initialValues} onFinish={onFinish} className="w-3/4">
+          <div className="mb-4">
+            <input
+              type="text"
+              placeholder="UserName"
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="email"
+              placeholder="Email Address"
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <div className="mb-6">
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full px-4 py-2 bg-blue-600 text-white font-bold rounded-lg hover:bg-blue-800 transition-all"
+          >
+            Register
+          </button>
+        </form>
+      </div>
     </div>
-    
-    {/* Right Section */}
-    <div className="w-1/2 bg-white flex flex-col justify-center items-center">
-      <h2 className="text-3xl font-bold mb-4">Hello!</h2>
-      <p className="mb-8">Sign Up to Get Started</p>
-      <form  initialValues={initialValues} onFinish={onFinish} className="w-3/4">
-        <div className="mb-4">
-          <input
-            type="text"
-            placeholder="UserName"
-            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-        </div>
-        <div className="mb-4">
-          <input
-            type="email"
-            placeholder="Email Address"
-            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-        </div>
-        <div className="mb-6">
-          <input
-            type="password"
-            placeholder="Password"
-            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full px-4 py-2 bg-blue-600 text-white font-bold rounded-lg hover:bg-blue-800 transition-all"
-        >
-          Register
-        </button>
-      </form>
-    </div>
-  </div>
   )
 }
 
